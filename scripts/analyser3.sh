f568b99275a5f3a4f91ffdfe6cff8cd8c971a50f
ErwinZorin <gza1275su@gmail.com>
2025-11-12 23:24:55 +0300

    Create analyser3.sh

---
+++ scripts/analyser3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in $(ls $1)
+do
+    for j in $(cat $1i)
+    do
+        age=$(echo $j | cut -d ":" -f2)
+        if [ $age -ge 18 ]
+        then
+            teen=$((teen+1))
+        else 
+            noteen=$((noteen+1))
+        fi
+    done
+done
+    
