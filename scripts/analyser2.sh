7e73de8a1496d5eddef394b0e0be5ca45fed7dea
ErwinZorin <gza1275su@gmail.com>
2025-11-12 23:07:29 +0300

    Update and rename script2 to analyser2.sh

---
+++ scripts/analyser2.sh
@@ -0,0 +1,11 @@
+Второй скприпт:
+
+#!/bin/bash
+for i in$(cat $1)
+do
+    for i in$(cat $1$i)
+    do
+        email=$(echo $j | cut -d ":" -f3)
+        echo $email | grep -o ".*@spbstu.ru"
+        done
+done
